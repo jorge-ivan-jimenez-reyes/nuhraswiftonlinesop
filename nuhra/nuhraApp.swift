03198fb1d29e8043c6146cecf5857aa691b698d7
jorge-ivan-jimenez-reyes <0251139@up.edu.mx>
2024-04-01 11:12:11 -0600

    Initial Commit

---
+++ nuhra/nuhraApp.swift
@@ -0,0 +1,17 @@
+//
+//  nuhraApp.swift
+//  nuhra
+//
+//  Created by Jorge Ivan JImenez Reyes on 19/03/24.
+//
+
+import SwiftUI
+
+@main
+struct nuhraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
