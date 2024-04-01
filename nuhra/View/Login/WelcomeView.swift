cb2147c41ef651ac64c0c057317a771bc01c9bf3
jorge-ivan-jimenez-reyes <0251139@up.edu.mx>
2024-04-01 13:14:36 -0600

    Added Fonts, Splash Screen, ViewImageScreen

---
+++ nuhra/View/Login/WelcomeView.swift
@@ -0,0 +1,25 @@
+//
+//  WelcomeView.swift
+//  nuhra
+//
+//  Created by Jorge Ivan JImenez Reyes on 01/04/24.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        ZStack{
+            Image("viewnuha")
+                .resizable()
+                .scaledToFill()
+            
+        }
+        .ignoresSafeArea()
+        
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
--- nuhra/nuhraApp.swift
+++ nuhra/nuhraApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct nuhraApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            WelcomeView()
         }
     }
 }
